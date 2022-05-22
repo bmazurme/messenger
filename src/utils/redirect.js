dbfdeec118fdc058a6d94a23260341803ccd892a
Bogdan <b@bmazur.ru>
2022-05-22 13:04:27 +0300

    fix: form

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -10,6 +10,7 @@ function redirectToProfile(evt) {
 function redirect() {
   const buttonSignin = document.querySelector('.identity_signin');
   const buttonSave = document.querySelector('.form_save');
+
   if (buttonSignin) {
     buttonSignin.addEventListener('submit', redirectToChat);
   }
